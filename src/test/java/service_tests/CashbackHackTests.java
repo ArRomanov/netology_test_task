4be162c07c3c2f1368aaf0db0dc9cf20e8076c6d
Artem Romanov <romanov-artyom@bk.ru>
2019-09-26 19:58:08 +0300

    Скорректировал java docs

--- src/test/java/service_tests/CashbackHackTests.java
+++ src/test/java/service_tests/CashbackHackTests.java
@@ -6,15 +6,27 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import service.CashbackHackService;
 
+/**
+ * Тесты для проверки CashbackHackService.
+ */
 class CashbackHackTests {
 
     private static CashbackHackService cashbackHackService;
 
+    /**
+     * Предварительная инициализация сервиса.
+     */
     @BeforeAll
-    static void createService(){
+    static void createService() {
         cashbackHackService = new CashbackHackService();
     }
 
+    /**
+     * Проверяет работу метода remain класса CashbackHackService.
+     *
+     * @param currentSumm      - текущая сумма
+     * @param expectedRestSumm - недостающая сумма для максимального кэшбека
+     */
     @ParameterizedTest(name = "{index} => currentSumm={0}, expectedRestSumm={1}")
     @CsvSource({
             "0, 1000",
@@ -23,7 +35,7 @@ class CashbackHackTests {
             "1364, 636",
             "2000, 0"
     })
-    void getRestSumm(int currentSumm, int expectedRestSumm){
+    void getRestSumm(int currentSumm, int expectedRestSumm) {
         int restSumm = cashbackHackService.remain(currentSumm);
         Assertions.assertEquals(expectedRestSumm, restSumm, "Оставшаяся сумма расчитана неверно!");
     }
